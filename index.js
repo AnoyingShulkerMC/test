6a48288362241f00b1112c4fc1a7a5b3dffd46f7
ryan20090101 <44982417+ryan20090101@users.noreply.github.com>
2018-11-15 13:52:05 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = function(r,c){
+try{
+const {parse} = require("querystring")
+var body = "";
+r.on("data",function(d){
+body += d.toString()
+})
+r.on("end",function(){
+c(parse(body))
+})
+}catch(err){
+c(undefined,err)
+}
+}
